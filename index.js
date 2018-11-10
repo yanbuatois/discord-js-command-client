a0cfffb8850fe7dd1793276b0f0de3cdbde3c92c
Yan BUATOIS <dracoctix@outlook.fr>
2018-11-10 12:46:55 +0100

    The bot can now run commands.

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const constants = require('util/constants.js');
 class CommandClient extends Client {
     /**
      * Class constructor
-     * @param {string} prefix Prefix for commands.
-     * @param {ClientOptions} options Options to be passed to the Client.
+     * @param {string} [prefix='!'] Prefix for commands.
+     * @param {ClientOptions} [options={}] Options to be passed to the Client.
      * @constructor
      */
     constructor(prefix = '!', options = {}) {
@@ -21,10 +21,17 @@ class CommandClient extends Client {
         this.prefix = prefix;
 
         /**
-         * All regitered commands
+         * All registered commands
          * @type {Object.<string, CommandInfos>}
          */
         this.registeredCommands = [];
+
+        /**
+         * Message displayed when usage doesn't have enough privileges to run command.
+         * @type {string}
+         * @default "You aren't allowed to run this command."
+         */
+        this.commandNotAllowedMessage = "You aren't allowed to run this command.";
 
         this.on("message", (message) => {
             if(message.content.startsWith(prefix)) {
@@ -32,8 +39,30 @@ class CommandClient extends Client {
                 const commandName = args[0];
                 args.shift();
 
-                if(Object.keys(this.registeredCommands).includes(commandName)) {
+                if(typeof this.registeredCommands[commandName] !== 'undefined') {
+                    const commandData = this.registeredCommands[commandName];
+                    const commandOptions = commandData.options;
+                    const isDm = (message.channel.type === 'dm' || message.channel.type === 'group');
 
+                    if(isDm && !commandOptions.dmAllowed)
+                        return;
+
+                    if(args.length < commandOptions.minArgs) {
+                        this._displayUsageMessage(message, commandData);
+                        return;
+                    }
+
+                    if(commandOptions.maxArgs >= 0 && args.length > commandOptions.maxArgs) {
+                        this._displayUsageMessage(message, commandData);
+                        return;
+                    }
+
+                    if(!isDm && commandOptions.requiredPermission !== 0 && message.channel.memberPermissions(message.author).has(commandOptions.requiredPermission)) {
+                        this._replyMessage(message, this.commandNotAllowedMessage);
+                        return;
+                    }
+
+                    commandData.callback(message, commandName, args);
                 }
             }
         })
@@ -42,21 +71,50 @@ class CommandClient extends Client {
     /**
      * Register a new command
      * @param {string} command Command without the prefix
-     * @param {function(message: Message,commandName: string,args: string[])} callback Callback function called when the command is triggered
+     * @param {CommandCallback} callback Callback function called when the command is triggered
      * @param {CommandOptions} options Options evaluated when the command is triggered
      */
     registerCommand(command, callback, options = constants.DefaultCommandOptions) {
         /**
          * Stores the informations about the command
-         * @typedef {{callback: (function(message: Message, commandName: string, args: string[])), options: CommandOptions}} CommandInfos
+         * @typedef {{name: string, callback: CommandCallback, options: CommandOptions}} CommandInfos
          */
         const commandInfos = {
+            name: command,
             callback: callback,
             options: options
         };
 
         this.registeredCommands[command] = commandInfos;
     }
+
+    /**
+     * Display the usage message if existing.
+     * @param {Message} message the original Discord message
+     * @param {CommandInfos} commandInfos
+     * @private
+     */
+    _displayUsageMessage(message, commandInfos) {
+        const options = commandInfos.options;
+        let usageMessage = options.usageMessage;
+        usageMessage.replace("%p", this.prefix);
+        usageMessage.replace("%f", this.prefix+commandInfos.name);
+        usageMessage.replace("%c", commandInfos.name);
+        this._replyMessage(message, "Usage : `" + usageMessage + "`")
+    }
+
+    /**
+     * Used to reply to a message, an check to avoid errors
+     * @param {Message} originalMessage Original message posted by user
+     * @param {string} messageToReply Content of the reply
+     * @private
+     */
+    _replyMessage(originalMessage, messageToReply) {
+        if(messageToReply !== '' && (message.channel.type === 'dm' || message.channel.type === 'group' || message.channel.memberPermissions(this.user).has("SEND_MESSAGES"))) {
+            originalMessage.reply(messageToReply);
+        }
+    }
+
 }
 
 exports.CommandClient = CommandClient;
